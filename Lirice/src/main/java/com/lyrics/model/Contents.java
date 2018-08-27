691bf9e64070e13f3030fbb192df5351dd38cfce
home <home@192.168.0.5>
2018-08-27 12:05:37 +0530

    update lyrics

---
+++ Lirice/src/main/java/com/lyrics/model/Contents.java
@@ -0,0 +1,30 @@
+package com.lyrics.model;
+
+import java.io.Serializable;
+
+public class Contents implements Serializable{
+
+	private int id;
+	private String content;
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
